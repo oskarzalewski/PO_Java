0d01decc786b6108e0349ef830c473208eb8bff7
oskarzalewski <77100545+oskarzalewski@users.noreply.github.com>
2022-01-30 23:37:48 +0100

    zad

---
+++ WordsInHashSetDemo/WordsInHashSetDemo.java
@@ -0,0 +1,30 @@
+package pl.ozalewski.po.WordsInHashSetDemo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class WordsInHashSetDemo {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Set<String> words = new TreeSet<>();
+        long totalTime = 0;
+        String path = "C:\\Users\\pc\\IdeaProjects\\laby\\src\\pl\\alice30.txt";
+        Scanner in = new Scanner(new File(path));
+        while (in.hasNext()) {
+            String word = in.next();
+            long callTime = System.currentTimeMillis();
+            words.add(word);
+            callTime = System.currentTimeMillis() - callTime;
+            totalTime += callTime;
+        }
+
+        Iterator<String> iter = words.iterator();
+        for (int j = 1; j <= 20; ++j) {
+            System.out.println(iter.next());
+        }
+
+        System.out.println(". . .");
+        System.out.println(words.size() + " unikalnych słów. " + totalTime + " milisekund.");
+    }
+}
